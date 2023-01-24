6ad5df30258e33fe9db661c5b3166a68948249a3
robinjack <robinmajack@gmail.com>
2023-01-24 18:13:16 +0000

    add the new setting

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -42,6 +42,7 @@ ignore_startup_parameters = ${PGBOUNCER_IGNORE_STARTUP_PARAMETERS}
 query_wait_timeout = ${PGBOUNCER_QUERY_WAIT_TIMEOUT:-120}
 max_db_connections = ${PGBOUNCER_MAX_DB_CONNECTIONS:-100}
 idle_transaction_timeout = ${PGBOUNCER_IDLE_TRANSACTION_TIMEOUT:-3600}
+verbose = ${PGBOUNCER_VERBOSE:-0}
 
 
 [databases]
